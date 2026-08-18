66e8d5f1b63466bc213dc98cf65b619440fdaedc
Muhammad-Tahir <mtz5prif@gmail.com>
2026-08-18 12:49:40 +0100

    add supabase skills

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,9 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 import { Database } from '../../database.types';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+const supabase = createClient<Database>(supabaseUrl, supabasePublishableKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
